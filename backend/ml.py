38e1ddcd895dfc6a6d19e6d35861b9b3b53d1d28
Matt Rabitsky <mRabitsky@gmail.com>
2019-10-13 03:05:43 -0700

    update the model situation

--- backend/ml.py
+++ backend/ml.py
@@ -1,4 +1,4 @@
-from catboost import CatBoost
+from catboost import CatBoostRegressor
 import json
 import datetime
 import re
@@ -14,26 +14,28 @@ def parse_y(d):
 
 
 # read the dataset
-with open('../data/extracted_data.json', 'r') as data_file:
+with open('../data/mock_data.json', 'r') as data_file:
     data = json.load(data_file)['data']
 
 train_data = list(map(parse_x, data))
 train_labels = list(map(parse_y, data))
-test_data = [35, 7, "Engineer", "Many but competitive"]
+test_data = [[60, 40 * 12, "Senior Engineer", "Many but competitive"], [60, 2 * 12, 'Senior Engineer', 'Many but competitive']]
 
 
-param = {'iterations': 5}
-model = CatBoost(param)
+param = {'iterations': 50}
+model = CatBoostRegressor(iterations=3, depth=8, learning_rate=1, loss_function='RMSE')
 # train the model
 model.fit(X=train_data, y=train_labels, cat_features=[2, 3])
 
-now = re.sub(r'[^0-9a-zA-Z]', '_', str(datetime.datetime.now()).split('.')[0])
-model.save_model(f'../data/model_{now}')
+# now = re.sub(r'[^0-9a-zA-Z]', '_', str(datetime.datetime.now()).split('.')[0])
+# model.save_model(f'../data/model_{now}')
+#
+# model_path = glob.glob('../data/model_*')[0]
+# new_model = CatBoost(param)
+# new_model.load_model(model_path)
+#
+# # make the prediction using the resulting model
+# preds_raw_vals = new_model.predict(test_data, prediction_type='RawFormulaVal')
+# print("Raw months:", preds_raw_vals)
 
-model_path = glob.glob('../data/model_*')[0]
-new_model = CatBoost(param)
-new_model.load_model(model_path)
-
-# make the prediction using the resulting model
-preds_raw_vals = new_model.predict(test_data, prediction_type='RawFormulaVal')
-print("Raw months:", preds_raw_vals)
+print("Raw months:", model.predict(test_data))
