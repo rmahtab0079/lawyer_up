9a5e2d8f790052f3aca4ccbb2152b874fa899e16
Matt Rabitsky <10282161+mRabitsky@users.noreply.github.com>
2019-10-12 23:49:51 -0700

    add "senior engineer" role

--- backend/categories.py
+++ backend/categories.py
@@ -1,3 +1,3 @@
-employment_type = ["C-Suite", "Manager", "Engineer", "Retail / Clerical / Administrative", "Sales", "Skilled Labour", "Unskilled Labour", "Upper Management", "Other", "Scientist"]
+employment_type = ["C-Suite", "Manager", "Engineer", "Senior Engineer", "Retail / Clerical / Administrative", "Sales", "Skilled Labour", "Unskilled Labour", "Upper Management", "Other", "Scientist"]
 employment_availability = ["Many but competitive", "Many and high demand", "Little to none", "Some"] 
 locations = ["Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Northwest Territories", "Nunavut", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan", "Yukon", "Canada"]
