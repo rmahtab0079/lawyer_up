4f914c1bb5a90d9a2d73ae27ce98543ccd264439
Matt Rabitsky <10282161+mRabitsky@users.noreply.github.com>
2019-10-12 23:44:32 -0700

    standardize formats
    I have so many typos

--- backend/categories.py
+++ backend/categories.py
@@ -1,3 +1,3 @@
-employment_type = ["Executive level management", "First tier manager or supervisor", "Middle management", "Professional", "Retail / clerical / administrative", "Sales", "Skilled labour", "Technical", "Unskilled labour", "Upper management", "Other"]
-employment_availability = ["many but competitive", "many and high demand", "little to none", "some"] 
+employment_type = ["C-Suite", "Manager", "Engineer", "Retail / Clerical / Administrative", "Sales", "Skilled Labour", "Unskilled Labour", "Upper Management", "Other", "Scientist"]
+employment_availability = ["Many but competitive", "Many and high demand", "Little to none", "Some"] 
 locations = ["Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador", "Nova Scotia", "Northwest Territories", "Nunavut", "Ontario", "Prince Edward Island", "Quebec", "Saskatchewan", "Yukon", "Canada"]
